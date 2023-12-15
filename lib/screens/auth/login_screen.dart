1d7d27a446781e11a8a47faa8956b6c760139ab6
s2xdnd2002s2 <102251300+s2xdnd2002s2@users.noreply.github.com>
2023-12-16 00:18:46 +0700

    BTL_Nhom5_Code
    BTL_Nhom5_Code

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../../api/apis.dart';
+import '../../helper/dialogs.dart';
+import '../../main.dart';
+import '../home_screen.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isAnimate = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    //for auto triggering animation
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() => _isAnimate = true);
+    });
+  }
+  // handles google login button click
+  _handleGoogleBtnClick() {
+    //for showing progress bar
+    Dialogs.showProgressBar(context);
+
+    _signInWithGoogle().then((user) async {
+      //for hiding progress bar
+      Navigator.pop(context);
+
+      if (user != null) {
+        log('\nUser: ${user.user}');
+        log('\nUserAdditionalInfo: ${user.additionalUserInfo}');
+
+        if ((await APIs.userExists())) {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+        } else {
+          await APIs.createUser().then((value) {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+          });
+        }
+      }
+    });
+  }
+  Future<UserCredential?> _signInWithGoogle() async {
+    try {
+      await InternetAddress.lookup('google.com');
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return await APIs.auth.signInWithCredential(credential);
+    } catch (e) {
+      log('\n_signInWithGoogle: $e');
+      Dialogs.showSnackbar(context, 'Something Went Wrong (Check Internet!)');
+      return null;
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 135,
+                    bottom: 42,
+                  ),
+                  child: Image.asset(
+                    'assets/images/img_background.png',
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 48,
+                  ),
+                  child: Text(
+                    'Connect easily with your family and friends over countries',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Text(
+            'Terms & Privacy Policy',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          InkWell(
+            onTap: () async {},
+            child: Container(
+              margin: const EdgeInsets.only(
+                top: 18,
+                bottom: 20,
+                left: 24,
+                right: 24,
+              ),
+              height: 52,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+
+                color: const Color(0xFF2196F3),
+
+              ),
+
+                child: ElevatedButton.icon(
+    style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.blue,
+    shape: const StadiumBorder(),
+    elevation: 1),
+
+                  onPressed: (){
+                    _handleGoogleBtnClick();
+                  },
+                  icon: Image.asset('assets/images/google.png', height: mq.height * .03 ),
+                  label: RichText(
+                      text: const TextSpan(
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                    children: [
+                      TextSpan(text:  "Start Messaging"),
+
+    ]),
+                  )
+                  ),
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
